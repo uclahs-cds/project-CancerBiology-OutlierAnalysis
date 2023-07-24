50962db89d9de02f40f80acd7fc931c30fe393d9
Amaan Jogia-Sattar <amaanjsattar@berkeley.edu>
2023-07-23 17:48:13 -0700

    renamed and edited files

--- META.KM.OUTLIERS.R
+++ META.KM.OUTLIERS.R
@@ -9,10 +9,10 @@ library(BoutrosLab.statistics.survival);
 library(BoutrosLab.plotting.survival);
 
 # Load necessary functions
-source('~/Desktop/BIGSUMMER.PROJ/KM_FUNCTIONS.R')
+# source('~/Desktop/BIGSUMMER.PROJ/KM_FUNCTIONS.R')
 
 # Load the RDA file
-load("/Users/amaanjsattar/Desktop/2023-07-07_Metabric_Outlier.rda");
+# load("/Users/amaanjsattar/Desktop/2023-07-07_Metabric_Outlier.rda");
 
 ### Data Cleaning ##################################################################################
 
@@ -30,10 +30,11 @@ meta.clinic.patient.metadata <- meta.clinic.patient[1:4,];
 # Remove the metadata from meta.clinic.patient.rm.
 meta.clinic.patient.rm <- meta.clinic.patient.rm[-c(1:4), ];
 
-
-# 1991 patients, 4782 outlier genes
-# sum up all of the columns to get per-patient outlier counts
-outlier.counts <- colSums(outlier.patient.tag.01);
+# sum up all of the columns to get per-patient outlier counts, and 
+# # create a new dataframe with indices as patient ID and values as outlier counts
+outlier.counts <- as.data.frame(
+    colSums(outlier.patient.tag.01)
+    );
 
 # create a new dataframe with 
     # indices corresponding to patient ID and values as per-patient outlier counts
@@ -53,7 +54,7 @@ rownames(outlier.counts.df) <- gsub(
     );
 
 # Get the common patient IDs between the two dataframes
-merged_data <- merge(
+merged.data <- merge(
     meta.clinic.patient.rm,
     outlier.counts.df,
     by = 0
@@ -61,25 +62,36 @@ merged_data <- merge(
 
 
 # Set the patient IDs as row names of the merged dataframe
-rownames(merged_data) <- merged_data$Row.names;
+rownames(merged.data) <- merged.data$Row.names;
 
 # Remove the redundant column containing patient IDs
-merged_data$Row.names <- NULL;
+merged.data$Row.names <- NULL;
 
-merged.data.surv <- merged_data[, c('Overall.Survival..Months.',
-                                    'Overall.Survival.Status',
-                                    'Pam50...Claudin.low.subtype',
+# Create a column with binary Survival Status in merged_data
+merged.data <- binary.survival(
+    merged.data, 
+    'Overall.Survival.Status',
+    'Overall.Survival.Binary'
+    );
+
+# Subset the dataframe to only contain necessary columns
+merged.data <- merged.data[, c('Overall.Survival..Months.',
+                                    'Overall.Survival.Binary',
                                     'Outliers')
                                 ];
-colnames(merged.data.surv)[3] <- 'Subtype'
 
+# Create Survival Object
+merged.data.surv <- create.surv(
+    merged.data, 
+    'Overall.Survival..Months.', 
+    'Overall.Survival.Binary'
+    );
 
+# Create a factor for the outlier groupings, making sure the levels correspond correctly
+outlier.groups <- factor(merged.data$Outliers,
+                         levels = unique(merged.data$Outliers));
 
-merged.data.surv <- subtype.cleaning(merged.data.surv, c('Basal',
-                                                         'claudin-low',
-                                                        'Her2',
-                                                        'LumA',
-                                                        'LumB',
-                                                        'Normal')
-                                     );
-                                  
+km.outliers.meta <- subtype.km.grouped(merged.data.surv,
+                                       outlier.groups,
+                                       'Kaplan-Meier Plot Grouped by Subtype',
+                                       '/Users/amaanjsattar/Desktop/testplot.tiff')
--- TCGA.FUNCTIONS.R
+++ TCGA.FUNCTIONS.R
@@ -22,7 +22,7 @@ library(BoutrosLab.statistics.survival);
 library(BoutrosLab.plotting.survival);
 
 # Load the RDA file
-load("/Users/amaanjsattar/Desktop/2023-07-07_TCGA_BRCA_Outlier.rda");
+# load("/Users/amaanjsattar/Desktop/2023-07-07_TCGA_BRCA_Outlier.rda");
 
 ### Data Cleaning ##################################################################################
 
