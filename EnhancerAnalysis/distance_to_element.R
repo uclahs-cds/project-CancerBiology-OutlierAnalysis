4ddc8fc60879d0da8535df76ff6fb4340a232f72
Julie Livingstone <jlivingstone@mednet.ucla.edu>
2024-04-04 14:03:39 -0700

    remove translocations from distance analysis

--- EnhancerAnalysis/distance_to_element.R
+++ EnhancerAnalysis/distance_to_element.R
@@ -8,7 +8,7 @@
 # 0.01		2024-02-28	jlivingstone	initial code
 # 0.02		2024-03-12	jlivingstone	add chromosome max value; heatmap
 # 0.03		2024-03-28	jlivingstone	calculate distance if gh element
-#						is within breakpoints
+#						is within breakpoints; remove TRA
 
 ### PREAMBLE ###########################################################################
 library(BoutrosLab.utilities)
@@ -45,18 +45,7 @@ inversion <- read.delim(
 inversion$submitted_sample_id <- sub('a2', 'a', inversion$submitted_sample_id)
 inversion$submitted_sample_id <- sub('a3', 'a', inversion$submitted_sample_id)
 
-translocation <- read.delim(
-	file = file.path(
-		'/hot/ref/cohort/ICGC/BRCA/EU/processed/wgs/',
-		'interchromosomal_rearrangement.BRCA-EU.tsv'
-		),
-	as.is = TRUE
-	)
-# to account for trailing numbers
-translocation$submitted_sample_id <- sub('a2', 'a', translocation$submitted_sample_id)
-translocation$submitted_sample_id <- sub('a3', 'a', translocation$submitted_sample_id)
-
-samples.with.muts <- union(unique(translocation$submitted_sample_id), unique(inversion$submitted_sample_id))
+samples.with.muts <- unique(inversion$submitted_sample_id)
 
 # read in outlier samples and genes
 outliers <- read.delim(
@@ -90,7 +79,7 @@ sample.overlap <- intersect(colnames(exprs), samples.with.muts)
 samples.to.include <- intersect(sample.overlap, outliers$sample.name)
 outliers.parsed <- outliers[match(samples.to.include, outliers$sample.name), ]
 
-# analysis on outlier level
+# analysis on outlier level; hypothesis
 # distance between mutation and gh element will be smaller in outlier patients
 
 # gh elements for outlier gene
@@ -141,7 +130,6 @@ get.distance <- function(gh.subset, bkpts, muts) {
 	return(outcome)
 	}
 
-# need to think of what to do for translocations - end of same chr ?
 distance.results <- list()
 for (j in 1:length(overlap.genes)) {
 	print(overlap.genes[j])
@@ -160,10 +148,7 @@ for (j in 1:length(overlap.genes)) {
 	# just keep the closest mutation / gh element
 	results <- data.frame()
 	for (i in 1:length(samples.to.include)) {
-		muts <- rbind(
-			inversion[inversion$submitted_sample_id %in% samples.to.include[i],],
-			translocation[translocation$submitted_sample_id %in% samples.to.include[i],]
-			)
+		muts <- inversion[inversion$submitted_sample_id %in% samples.to.include[i],]
 
 		#start
 		from.bkpt <- GRanges(
@@ -207,7 +192,7 @@ for (j in 1:length(overlap.genes)) {
 
 save(
 	distance.results,
-	file = generate.filename('outlier', 'distance_of_mutation_to_outlier_gene_gh_element_modified', 'rda')
+	file = generate.filename('outlier', 'distance_of_mutation_to_outlier_gene_gh_element_inversions', 'rda')
 	)
 
 # different scenarios - no mutation on same chromosome so no distance returned)
