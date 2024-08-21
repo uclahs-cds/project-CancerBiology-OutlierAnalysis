7758393039811ae30637e031ce57cfa34c83367c
Julie Livingstone <jlivingstone@mednet.ucla.edu>
2024-08-21 07:07:44 -0700

    update run_outseekr script

--- test_OutSeekR/run_outseekr.R
+++ test_OutSeekR/run_outseekr.R
@@ -8,36 +8,39 @@
 
 ### PREAMBLE ###########################################################################
 library(OutSeekR)
+library(devtools)
 
 set.seed(12345)
+
+setwd('/hot/project/process/CancerBiology/OUTA-000164-GeneExpressionOABRCA/jlivingstone/test_outseekr')
+
+load_all('/hot/code/jlivingstone/GitHub/uclahs-cds/package-OutSeekR/')
 
 future::plan(future::multisession)
 
 # TCGA Uterine (5245 x 58)
-#exprs <- read.delim(
-#	file = file.path(
-#		'/hot/project/process/CancerBiology/OUTA-000164-GeneExpressionOABRCA/jlivingstone/test_outseekr',
-#		'2024-07-30_outlier_GDC_TCGA-UCS_processed_TPM.tsv'
-#		),
-#	as.is = TRUE,
-#	row.names = 1
-#	)
-
-brca <- read.delim(
+exprs <- read.delim(
 	file = file.path(
-		'/hot/project/process/CancerBiology/OUTA-000164-GeneExpressionOABRCA/jlivingstone/NikZainal_2016/processed',
-		'2023-11-14_BRCA_EU_transcriptomics_342.tsv'
+		'/hot/project/process/CancerBiology/OUTA-000164-GeneExpressionOABRCA/jlivingstone/test_outseekr',
+		'2024-07-30_outlier_GDC_TCGA-UCS_processed_TPM.tsv'
 		),
 	as.is = TRUE,
 	row.names = 1
 	)
 
-gene.var <- apply(brca, 1, function(x) { var(as.numeric(x), na.rm = TRUE) })
-exprs <- brca[which(gene.var > 3),]
-exprs[is.na(exprs)] <- 0
+# values are non-logged
+brca <- read.delim(
+	file = file.path(
+		'/hot/project/process/CancerBiology/OUTA-000164-GeneExpressionOABRCA/jlivingstone/NikZainal_2016/processed',
+		'2024-08-14_BRCA_EU_processed_unlogged.tsv'
+		),
+	as.is = TRUE
+	)
+#dim(brca)
+#[1] 17696   342
 
 outliers <- detect.outliers(
-	data = exprs,
+	data = brca,
 	num.null = 1000,
 	p.value.threshold = 0.05,
 	fdr.threshold = 0.1
