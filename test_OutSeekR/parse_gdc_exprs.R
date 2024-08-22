b141d75569435068ed69dd27604c311a700852e7
Julie Livingstone <jlivingstone@mednet.ucla.edu>
2024-08-22 08:14:24 -0700

    update parsing script to filtered based on % of sample detected in; file paths

--- test_OutSeekR/parse_gdc_exprs.R
+++ test_OutSeekR/parse_gdc_exprs.R
@@ -1,12 +1,13 @@
-## parse_gdc_exprs.R ##################################################################
+## parse_gdc_exprs.R ###################################################################################
 # Description
 # parse UCS (Uterine) GDC expression data 
 
-### HISTORY ############################################################################
+### HISTORY ############################################################################################
 # Version	Date		Developer	Comments
 # 0.01		2024-07-29	jlivingstone	initial code
+# 0.02		2024-08-22	jlivingstone	update to include genes detected in 50% of samples
 
-### PREAMBLE ###########################################################################
+### PREAMBLE ###########################################################################################
 library(BoutrosLab.utilities)
 
 setwd('/hot/project/process/CancerBiology/OUTA-000164-GeneExpressionOABRCA/jlivingstone/test_outseekr')
@@ -36,19 +37,42 @@ for (i in 1:ncol(tpm)) {
 colnames(tpm) <- tpm.samples
 
 # since this is a test and we want to reduce the number of genes
-# lets apply a variation cutoff > 1
-gene.var <- apply(log2(tpm + 1), 1, var, na.rm = TRUE)
-tokeep <- which(gene.var > 1)
+# detected in 50% of samples
+shadow.matrix <- tpm
 
-exprs <- data.frame(
-	ensembl = data$gene_id[tokeep],
-	log2(tpm + 1)[tokeep,]
-	)
+# abundance below TPM = 1 is considered not detected
+shadow.matrix[shadow.matrix < 1] <- 0
+
+shadow.matrix[shadow.matrix > 1] <- 1
+sample.count <- rowSums(shadow.matrix)
+
+min.count <- round(ncol(shadow.matrix) * 0.5)
+tokeep <- which(sample.count > min.count)
+
+exprs <- tpm[tokeep,]
+rownames(exprs) <- data$gene_id[tokeep]
 
 write.table(
 	x = exprs,
 	file = generate.filename('outlier', 'GDC_TCGA-UCS_processed_TPM', 'tsv'),
 	quote = FALSE,
 	sep = '\t',
-	row.names = FALSE
+	row.names = TRUE
+	)
+
+# or based on variance
+# lets apply a log2 variation cutoff > 1
+# Note: OutSeekR wants unlogged values
+gene.var <- apply(log2(tpm + 1), 1, var, na.rm = TRUE)
+tokeep.var <- which(gene.var > 1)
+ 
+exprs.var <- tpm[tokeep.var,]
+rownames(exprs.var) <- data$gene_id[tokeep.var]
+
+write.table(
+	x = exprs.var,
+	file = generate.filename('outlier', 'GDC_TCGA-UCS_processed_variance_TPM', 'tsv'),
+	quote = FALSE,
+	sep = '\t',
+	row.names = TRUE
 	)
--- test_OutSeekR/run_outseekr.R
+++ test_OutSeekR/run_outseekr.R
@@ -18,14 +18,25 @@ load_all('/hot/code/jlivingstone/GitHub/uclahs-cds/package-OutSeekR/')
 
 future::plan(future::multisession)
 
-# TCGA Uterine (5245 x 58)
+# TCGA Uterine (17247 x 57) - filtered for genes detected in 50% of samples
 exprs <- read.delim(
 	file = file.path(
 		'/hot/project/process/CancerBiology/OUTA-000164-GeneExpressionOABRCA/jlivingstone/test_outseekr',
-		'2024-07-30_outlier_GDC_TCGA-UCS_processed_TPM.tsv'
+		'2024-08-22_outlier_GDC_TCGA-UCS_processed_TPM.tsv'
 		),
-	as.is = TRUE,
-	row.names = 1
+	as.is = TRUE
+	)
+
+ucs.outliers <- detect.outliers(
+	data = exprs,
+	num.null = 1000,
+	p.value.threshold = 0.05,
+	fdr.threshold = 0.1
+	)
+
+save(
+	ucs.outliers,
+	file = genereate.filename('outliers', 'GDC_TCGA-UCS_results_using_package_function', 'rda')
 	)
 
 # values are non-logged
@@ -44,6 +55,11 @@ outliers <- detect.outliers(
 	num.null = 1000,
 	p.value.threshold = 0.05,
 	fdr.threshold = 0.1
+	)
+
+save(
+	outliers,
+	file = genereate.filename('outliers', 'BRCA_EU_results_using_package_function', 'rda')
 	)
 
 future::plan(future::sequential)
