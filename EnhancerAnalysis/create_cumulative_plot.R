803fa465620b6b08995469c4b2a6af9c02d11b02
Julie Livingstone <jlivingstone@mednet.ucla.edu>
2024-09-04 10:14:29 -0700

    update launch script; add script to create cumulative plot

---
+++ EnhancerAnalysis/create_cumulative_plot.R
@@ -0,0 +1,70 @@
+## deletion_analysis.R #################################################################
+# Description
+# create cumulative plot to show the permutation test results
+
+### HISTORY ############################################################################
+# Version	Date		Developer	Comments
+# 0.01		2024-09-04	jlivingstone	initial code
+
+### PREAMBLE ###########################################################################
+library(BoutrosLab.utilities)
+
+setwd('/hot/project/process/CancerBiology/OUTA-000164-GeneExpressionOABRCA/jlivingstone/enhancer_analysis/permutation')
+
+files <- list.files(
+	pattern = 'permutation_results_bkpt_padding_1000_overlap.txt'
+	)
+
+results <- NULL
+for (i in 1:length(files)) {
+	data <- read.delim(
+		file = file.path(
+			'/hot/project/process/CancerBiology/OUTA-000164-GeneExpressionOABRCA/jlivingstone/enhancer_analysis/permutation',
+			files[i]
+			),
+		as.is = TRUE
+		)
+	results <- rbind(results, data)
+	}
+results$n.overlap[is.na(results$n.overlap)] <- 0
+
+ecdf.data <- ecdf(results$n.overlap)
+
+count <- knots(ecdf.data)
+ecdf.plot.data <- data.frame(
+	overlap.count = count,
+	cumulative.probability = ecdf.data(count)
+	)
+
+create.scatterplot(
+	formula = cumulative.probability ~ overlap.count,
+	data = ecdf.plot.data,
+	file = generate.filename('outlier', 'enhancer_permutation_cumulative_plot', 'png'),
+	xlab.label = expression('Overlap count'),
+	ylab.label = expression('Cumulative Probability'),
+	col = 'grey10',
+	lwd = 3,
+	abline.v = 4, 
+	abline.lty = 2,
+	abline.lwd = 1.5,
+	abline.col = 'red2',
+	main.cex = 0,
+	xaxis.cex = 1,
+	yaxis.cex = 1,
+	yaxis.tck = c(0.2, 0),
+	xaxis.tck = c(0.2, 0),
+	xlab.cex = 1.3,
+	ylab.cex = 1.5,
+	xat = seq(0, 8, 1),
+	yat = seq(0, 1, 0.2),
+	xaxis.lab = c(0:8),
+	xlimits = c(-0.2, 8.5),
+	ylimits = c(0, 1.15),
+	xaxis.fontface = 1,
+	yaxis.fontface = 1,
+	add.grid = TRUE,
+	xgrid.at = c(2, 4, 6, 8),
+	ygrid.at = c(0.2, 0.4, 0.6, 0.8, 1),
+	grid.colour = 'grey80',
+	type = 'l'
+	)
--- EnhancerAnalysis/launch_permutation.sh
+++ EnhancerAnalysis/launch_permutation.sh
@@ -3,11 +3,12 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
 #SBATCH --mem=2G
-#SBATCH --array=0-4
+#SBATCH --array=1-9
+#SBATCH --nodelist=F16-5
 
 date
-seed_start=(32000 34000 36000 38000 40000)
-seed_end=(33999 35999 37999 39999 41999)
+seed_start=(90000 91000 92000 93000 94000 95000 96000 97000 98000 99000)
+seed_end=(90999 91999 92999 93999 94999 95999 96999 97999 98999 99999)
 Rscript /hot/code/jlivingstone/GitHub/uclahs-cds/project-CancerBiology-OutlierAnalysis/EnhancerAnalysis/permutation_test.R \
         --seed.start ${seed_start[$SLURM_ARRAY_TASK_ID]} \
         --seed.end ${seed_end[$SLURM_ARRAY_TASK_ID]} \
