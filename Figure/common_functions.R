bb818a38840b58afff5c10fe84b442f129c6ef4b
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2024-10-07 14:35:30 -0700

    Add loading and caching functions for variables

--- Figure/common_functions.R
+++ Figure/common_functions.R
@@ -71,3 +71,52 @@ save.outlier.figure <- function(plot.object, name.segments, width, height, outpu
     dev.off();
     return(invisible(NULL));
     }
+
+### cache.computed.variable #######################################################################
+# Description:
+#
+# Cache a variable for use in a later script. This deliberately only caches a
+# single variable at a time. The variable can be re-loaded into the local
+# environment with load.computed.variable.
+#
+# Input variables:
+#
+#   object.name: The name of the variable to be cached.
+cache.computed.variable <- function(object.name) {
+    output.directory <- here::here('variable-cache');
+
+    if (!dir.exists(output.directory)) {
+        dir.create(output.directory);
+        }
+
+    data.file <- file.path(output.directory, paste0(object.name, '.rda'));
+    save(list = c(object.name), file = data.file);
+    }
+
+
+cache.multiple.computed.variables <- function(object.names) {
+    for (name in object.names) {
+        cache.computed.variable(name);
+        }
+    }
+
+### load.computed.variable #######################################################################
+# Description:
+#
+# Reload a single variable computed in a prior script.
+#
+# Input variables:
+#
+#   object.name: The name of the variable to be loaded
+load.computed.variable <- function(object.name) {
+    data.file <- file.path(here::here('variable-cache'), paste0(object.name, '.rda'));
+    load(data.file);
+    assign(object.name, get(object.name), envir = parent.frame());
+}
+
+load.multiple.computed.variables <- function(object.names) {
+    for (name in object.names) {
+        load.computed.variable(name);
+        assign(name, get(name), envir = parent.frame());
+        }
+    }
