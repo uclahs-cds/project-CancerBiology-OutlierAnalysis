39f1cd078365f2efef9907a9c0cf0b9f0d83fe44
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2024-10-14 10:47:28 -0700

    Merge more plots

--- Figure/Figure4/Figure4lm.R
+++ Figure/Figure4/Figure4lm.R
@@ -219,13 +219,6 @@ sanger.zscore.drug.breast.match.non.df <- bind_rows(sanger.zscore.drug.breast.ma
 sanger.zscore.drug.breast.match.info.df <- bind_rows(sanger.zscore.drug.breast.match.info)
 
 
-cache.multiple.computed.variables(c(
-    'ccle.sample.outlier.status.overlap.na.samger.match.dup.filter',
-    'depmap.drug.info.match.sanger.dup',
-    'sanger.drug.match.dup.zscore',
-    'sanger.zscore.drug.breast.match.info.df'
-    ));
-
 sanger.zscore.drug.breast.match.out.list.mean <- lapply(sanger.zscore.drug.breast.match.out, function(x) {
     mean(na.omit(as.numeric(unlist(x))))
     })
@@ -371,9 +364,80 @@ zscore.box.sanger <- BoutrosLab.plotting.general::create.boxplot(
 
 save.outlier.figure(
     zscore.box.sanger,
-    c('Figure4l', 'sanger', 'drug', 'box'),
+    c('Figure4lm', 'sanger', 'drug', 'box'),
     width = 4,
     height = 6
     );
 
-save.session.profile(file.path('output', 'Figure4l.txt'));
+i <- 'SYK';
+
+drug.target <- sanger.zscore.drug.breast.match.info.df[sanger.zscore.drug.breast.match.info.df$repurposing_target %in% i, ]$Drug.Name;
+
+# Get patient status and drug target information
+patient.status <- ccle.sample.outlier.status.overlap.na.samger.match.dup.filter[i, ];
+drug.target <- depmap.drug.info.match.sanger.dup[depmap.drug.info.match.sanger.dup$repurposing_target %in% i, ];
+
+# Get IC50 z-score values for the selected gene
+ic50.value.i <- sanger.drug.match.dup.zscore[match(drug.target$Drug.Name, rownames(sanger.drug.match.dup.zscore)), colnames(patient.status), drop = FALSE];
+# remove duplicated row
+ic50.value.i <- ic50.value.i[c(1, 3, 4), ];
+
+# Create a data frame for plotting
+i.drug.box <- data.frame(
+    score = unlist(data.frame(t(ic50.value.i))),
+    status = rep(unlist(patient.status), 3),
+    group = c(
+        rep('a', length(patient.status)),
+        rep('b', length(patient.status)),
+        rep('c', length(patient.status))
+        )
+    );
+
+# Define colors for the points based on the patient status
+dot.colours <- rep('grey70', nrow(i.drug.box));
+dot.colours[i.drug.box$status == 1] <- 'green4';
+
+# Establish an arbitrary but consistent random seed for plotting consistency
+set.seed(sum(utf8ToInt('Figure4m')));
+
+# Generate the boxplot
+i.drug.box.plot <- BoutrosLab.plotting.general::create.boxplot(
+    formula = score ~ group,
+    data = i.drug.box,
+    main = expression('Gene effect score of outlier genes'),
+    outlier = TRUE,
+    add.stripplot = TRUE,
+    # jitter.factor = 10,
+    main.cex = 1.5,
+    xlab.label = NULL,
+    xlab.cex = 0,
+    ylab.label = expression('z-score'),
+    ylab.cex = 1.3,
+    yaxis.cex = 1.1,
+    xaxis.cex = 1.1,
+    xaxis.lab = rownames(ic50.value.i),
+    xaxis.fontface = 1,
+    yaxis.fontface = 1,
+    yaxis.tck = c(0.2, 0),
+    xaxis.tck = c(0.2, 0),
+    xaxis.rot = 90,
+    # ylimits = c(-1.7,2.5),
+    ylimits = c(-2.05, 3.3),
+    yat = seq(-2, 3, 1),
+    sample.order = 'none',
+    points.pch = 16,
+    points.cex = 1,
+    points.col = dot.colours,
+    lwd = 1.2,
+    col = 'gold2',
+    alpha = 0.25
+    );
+
+save.outlier.figure(
+    i.drug.box.plot,
+    c('Figure4lm', i, 'box', 'drug'),
+    width = 5,
+    height = 6
+    );
+
+save.session.profile(file.path('output', 'Figure4lm.txt'));
